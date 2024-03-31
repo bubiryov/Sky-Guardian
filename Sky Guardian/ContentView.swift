3bad50f653f93ef22439248f16bf7bc5872ab19f
bubiryov <bubiryov@icloud.com>
2024-03-31 16:23:12 +0300

    Initial Commit

---
+++ Sky Guardian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sky Guardian
+//
+//  Created by Egor Bubiryov on 31.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sky Guardian/Sky_GuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sky_GuardianApp.swift
+//  Sky Guardian
+//
+//  Created by Egor Bubiryov on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sky_GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
